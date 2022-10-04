d230201715279e2e004dff2e7370f8c42798b98d
Yurino Oga <yurino.o@latona.io>
2022-10-04 23:06:35 +0900

    release

---
+++ data-platform-price-master-sql-price-master-data.sql
@@ -0,0 +1,32 @@
+CREATE TABLE `data_platform_price_master_price_master_data`
+(
+    `Product`                     varchar(40) NOT NULL,
+    `SalesOrPurchaseOrganization` varchar(40) NOT NULL,
+    `DistributionChannel`         varchar(40) NOT NULL,
+    `BusinessPartner`             varchar(40) NOT NULL,
+    `ConditionRecord`             varchar(40) NOT NULL,
+    `ConditionSequentialNumber`   varchar(40) NOT NULL,
+    `ConditionType`               varchar(40) NOT NULL,
+    `ConditionValidityEndDate`    varchar(40) NOT NULL,
+    `ConditionValidityStartDate`  varchar(40) NOT NULL,
+    `CreationDate`                varchar(40) NOT NULL,
+    `ConditionRateValue`          varchar(40) NOT NULL,
+    `ConditionRateValueUnit`      varchar(40) NOT NULL,
+    `ConditionRateRatio`          varchar(40) NOT NULL,
+    `ConditionRateRatioUnit`      varchar(40) NOT NULL,
+    `ConditionRateAmount`         varchar(40) NOT NULL,
+    `ConditionCurrency`           varchar(40) NOT NULL,
+    `ConditionQuantity`           varchar(40) NOT NULL,
+    `ConditionQuantityUnit`       varchar(40) NOT NULL,
+    `BaseUnit`                    varchar(40) NOT NULL,
+    `ConditionLowerLimit`         varchar(40) NOT NULL,
+    `ConditionLowerLimitAmount`   varchar(40) NOT NULL,
+    `ConditionLowerLimitRatio`    varchar(40) NOT NULL,
+    `ConditionUpperLimit`         varchar(40) NOT NULL,
+    `ConditionUpperLimitAmount`   varchar(40) NOT NULL,
+    `ConditionUpperLimitRatio`    varchar(40) NOT NULL,
+    `ConditionIsDeleted`          varchar(40) NOT NULL,
+    `PaymentTerms`                varchar(40) NOT NULL,
+     PRIMARY KEY (`Price`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;

3bc2497de355ec2b621bfe3e1bd58a3abedae794
Yurino Oga <yurino.o@latona.io>
2023-01-01 14:24:45 +0900

    release

--- data-platform-price-master-sql-price-master-data.sql
+++ data-platform-price-master-sql-price-master-data.sql
@@ -1,17 +1,15 @@
 CREATE TABLE `data_platform_price_master_price_master_data`
 (
-    `BusinessPartner`             int(12) NOT NULL,
-    `ConditionRecordCategory`     varchar(2) NOT NULL,
+    `SupplyChainRelationshipID`   int(16) NOT NULL,
+    `Buyer`                       int(12) NOT NULL,
+    `Seller`                      int(12) NOT NULL,
+--  `ConditionRecordCategory`     varchar(2) NOT NULL,
     `ConditionRecord`             int(12) NOT NULL,
     `ConditionSequentialNumber`   int(2) NOT NULL,
-    `ConditionType`               varchar(4) NOT NULL,
     `ConditionValidityEndDate`    date NOT NULL,
     `ConditionValidityStartDate`  date NOT NULL,
     `Product`                     varchar(40) DEFAULT NULL,
---  `SalesOrPurchaseOrganization` varchar(4) DEFAULT NULL,
---  `DistributionChannel`         varchar(2) DEFAULT NULL,
-    `Customer`                    int(12) DEFAULT NULL,
-    `Supplier`                    int(12) DEFAULT NULL,
+    `ConditionType`               varchar(4) NOT NULL,
     `CreationDate`                date DEFAULT NULL,
     `ConditionRateValue`          float(13) DEFAULT NULL,
     `ConditionRateValueUnit`      varchar(3) DEFAULT NULL,
@@ -21,12 +19,10 @@ CREATE TABLE `data_platform_price_master_price_master_data`
     `BaseUnit`                    varchar(3) DEFAULT NULL,
     `ConditionIsDeleted`          tinyint(1) DEFAULT NULL,
     
-     PRIMARY KEY (`BusinessPartner`, `ConditionRecordCategory`, `ConditionRecord`, `ConditionSequentialNumber`, `ConditionType`, `ConditionValidityEndDate`, `ConditionValidityStartDate`, `Product`),
-     
-    CONSTRAINT `DataPlatformPriceMasterPriceMasterDataBusinessPartner_fk` FOREIGN KEY (`BusinessPartner`) REFERENCES `data_platform_business_partner_general_data` (`BusinessPartner`),
+    PRIMARY KEY (`SupplyChainRelationshipID`, `Buyer`, `Seller`, `ConditionRecord`, `ConditionSequentialNumber`, `Product`, `ConditionValidityEndDate`, `ConditionValidityStartDate`),
+
+    CONSTRAINT `DataPlatformPriceMasterPriceMasterDataSupplyChainRelationshipID_fk` FOREIGN KEY (`SupplyChainRelationshipID`, `Buyer`, `Seller`) REFERENCES `data_platform_supply_chain_relationship_general_data` (`SupplyChainRelationshipID`, `Buyer`, `Seller`),
     CONSTRAINT `DataPlatformPriceMasterPriceMasterDataProduct_fk` FOREIGN KEY (`Product`) REFERENCES `data_platform_product_master_general_data` (`Product`),
-    CONSTRAINT `DataPlatformPriceMasterPriceMasterDataCustomer_fk` FOREIGN KEY (`BusinessPartner`, `Customer`) REFERENCES `data_platform_business_partner_customer_data` (`BusinessPartner`, `Customer`),
-    CONSTRAINT `DataPlatformPriceMasterPriceMasterDataSupplier_fk` FOREIGN KEY (`BusinessPartner`, `Supplier`) REFERENCES `data_platform_business_partner_supplier_data` (`BusinessPartner`, `Supplier`),
     CONSTRAINT `DataPlatformPriceMasterPriceMasterDataConditionRateValueUnit_fk` FOREIGN KEY (`ConditionRateValueUnit`) REFERENCES `data_platform_quantity_unit_quantity_unit_data` (`QuantityUnit`),
     CONSTRAINT `DataPlatformPriceMasterPriceMasterDataConditionRateRatioUnit_fk` FOREIGN KEY (`ConditionRateRatioUnit`) REFERENCES `data_platform_quantity_unit_quantity_unit_data` (`QuantityUnit`),
     CONSTRAINT `DataPlatformPriceMasterPriceMasterDataConditionCurrency_fk` FOREIGN KEY (`ConditionCurrency`) REFERENCES `data_platform_currency_currency_data` (`Currency`),
